293fe340c594e3f03d9618371c5415f0d136d096
Hugh Do <mhughdo@gmail.com>
2021-04-11 11:39:07 +0700

    add: much faster build using Esbuild-loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,46 @@
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
+function useEsbuildMinify(config, options) {
+	const terserIndex = config.optimization.minimizer.findIndex(minimizer => (minimizer.constructor.name === 'TerserPlugin'));
+	if (terserIndex > -1) {
+		config.optimization.minimizer.splice(
+			terserIndex,
+			1,
+			new ESBuildMinifyPlugin(options),
+		);
+	}
+}
+
+
+function useEsbuildLoader(config, options) {
+	const jsLoader = config.module.rules.find(rule => rule.test && rule.test.test('.ts'));
+
+	if (jsLoader) {
+		jsLoader.use.loader = 'esbuild-loader';
+		jsLoader.use.options = options;
+	}
+}
+
 module.exports = {
   future: {
     webpack5: true,
   },
-   distDir: 'out',
+  webpack: (config, { webpack }) => {
+		config.plugins.push(
+			new webpack.ProvidePlugin({
+				React: 'react',
+			}),
+		);
+
+		useEsbuildMinify(config);
+
+		useEsbuildLoader(config, {
+			loader: 'tsx',
+			target: 'es6',
+		});
+
+		return config;
+	},
+  distDir: 'out',
   target: "experimental-serverless-trace",
 }
